c4a019103a4b2e984f11763af7d9b40af70b6192
Francis Hilaire <Prometee@users.noreply.github.com>
2024-10-31 12:50:36 +0100

    Add payment request pay route to be able to finalize a capture or an authorize request

--- DependencyInjection/SyliusShopExtension.php
+++ DependencyInjection/SyliusShopExtension.php
@@ -127,6 +127,8 @@ final class SyliusShopExtension extends Extension implements PrependExtensionInt
 
     private function configureOrderPay(array $config, ContainerBuilder $container): void
     {
+        $container->setParameter('sylius_shop.order_pay.payment_request_pay_route', $config['payment_request_pay_route']);
+        $container->setParameter('sylius_shop.order_pay.payment_request_pay_route_parameters', $config['payment_request_pay_route_parameters']);
         $container->setParameter('sylius_shop.order_pay.after_pay_route', $config['after_pay_route']);
         $container->setParameter('sylius_shop.order_pay.after_pay_route_parameters', $config['after_pay_route_parameters']);
         $container->setParameter('sylius_shop.order_pay.final_route', $config['final_route']);
