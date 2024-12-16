e6c5db7aa3ebb6e580a734fabe6cc00a2a87cd08
Bartłomiej Sęk <bartlomiej.sek@bitbag.pl>
2024-12-16 13:41:15 +0100

    Lint project files

--- index.js
+++ index.js
@@ -11,30 +11,30 @@ const path = require('path');
 const Encore = require('@symfony/webpack-encore');
 
 class SyliusShop {
-  static getWebpackConfig(rootDir) {
-    Encore
-      .setOutputPath('public/build/shop/')
-      .setPublicPath('/build/shop')
-      .addEntry('shop-entry', path.resolve(__dirname, 'Resources/assets/entrypoint.js'))
-      .disableSingleRuntimeChunk()
-      .cleanupOutputBeforeBuild()
-      .enableSourceMaps(!Encore.isProduction())
-      .enableVersioning(Encore.isProduction())
-      .enableSassLoader((options) => {
-        // eslint-disable-next-line no-param-reassign
-        options.additionalData = `$rootDir: '${rootDir}';`;
-      })
-      .enableStimulusBridge(path.resolve(__dirname, 'Resources/assets/controllers.json'));
+    static getWebpackConfig(rootDir) {
+        Encore
+            .setOutputPath('public/build/shop/')
+            .setPublicPath('/build/shop')
+            .addEntry('shop-entry', path.resolve(__dirname, 'Resources/assets/entrypoint.js'))
+            .disableSingleRuntimeChunk()
+            .cleanupOutputBeforeBuild()
+            .enableSourceMaps(!Encore.isProduction())
+            .enableVersioning(Encore.isProduction())
+            .enableSassLoader((options) => {
+                // eslint-disable-next-line no-param-reassign
+                options.additionalData = `$rootDir: '${rootDir}';`;
+            })
+            .enableStimulusBridge(path.resolve(__dirname, 'Resources/assets/controllers.json'));
 
-    const shopConfig = Encore.getWebpackConfig();
+        const shopConfig = Encore.getWebpackConfig();
 
-    shopConfig.externals = { ...shopConfig.externals, window: 'window', document: 'document' };
-    shopConfig.name = 'shop';
+        shopConfig.externals = { ...shopConfig.externals, window: 'window', document: 'document' };
+        shopConfig.name = 'shop';
 
-    Encore.reset();
+        Encore.reset();
 
-    return shopConfig;
-  }
+        return shopConfig;
+    }
 }
 
 module.exports = SyliusShop;
