6cb6229e09444ff672cc69b53c7ac6b8710b2ca7
Jibé Barth <barth.jib@gmail.com>
2025-05-03 18:29:39 +0200

    feat(stimulus): Switch to a bundle named namespace
    Require stimulus controller through controllers.json
    Do not add them by default on webpack config for both admin/shop

--- index.js
+++ index.js
@@ -12,19 +12,9 @@ const Encore = require('@symfony/webpack-encore');
 
 class SyliusShop {
     static getWebpackConfig(rootDir) {
+        this._prepareWebpackConfig(rootDir);
         Encore
-            .setOutputPath('public/build/shop/')
-            .setPublicPath('/build/shop')
             .addEntry('shop-entry', path.resolve(__dirname, 'Resources/assets/entrypoint.js'))
-            .disableSingleRuntimeChunk()
-            .cleanupOutputBeforeBuild()
-            .enableSourceMaps(!Encore.isProduction())
-            .enableVersioning(Encore.isProduction())
-            .enableSassLoader((options) => {
-                // eslint-disable-next-line no-param-reassign
-                options.additionalData = `$rootDir: '${rootDir}';`;
-            })
-            .enableStimulusBridge(path.resolve(__dirname, 'Resources/assets/controllers.json'));
 
         const shopConfig = Encore.getWebpackConfig();
 
@@ -34,6 +24,36 @@ class SyliusShop {
         Encore.reset();
 
         return shopConfig;
+    }
+
+    static _getInternalWebpackConfig(rootDir) {
+        this._prepareWebpackConfig(rootDir);
+        // For a ready-to-use Stimulus bridge. Should be used only for sylius/sylius tests
+        Encore
+            .addEntry('shop-entry', path.resolve(__dirname, 'Resources/assets/app.js'))
+            .enableStimulusBridge(path.resolve(__dirname, 'Resources/assets/controllers.json'));
+        const shopConfig = Encore.getWebpackConfig();
+
+        shopConfig.externals = { ...shopConfig.externals, window: 'window', document: 'document' };
+        shopConfig.name = 'shop';
+
+        Encore.reset();
+
+        return shopConfig;
+    }
+
+    static _prepareWebpackConfig(rootDir) {
+        Encore
+            .setOutputPath('public/build/shop/')
+            .setPublicPath('/build/shop')
+            .disableSingleRuntimeChunk()
+            .cleanupOutputBeforeBuild()
+            .enableSourceMaps(!Encore.isProduction())
+            .enableVersioning(Encore.isProduction())
+            .enableSassLoader((options) => {
+                // eslint-disable-next-line no-param-reassign
+                options.additionalData = `$rootDir: '${rootDir}';`;
+            })
     }
 }
 
